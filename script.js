f50bdb0525ede1d11b1e3069ef356fd3a376ea28
Mert-Ince-lab <mertince2322@gmail.com>
2025-10-28 21:46:08 +0100

    addet few classes

--- script.js
+++
@@ -1,7 +0,0 @@
-function logUp(){console.log("logUp");}
-function logDown(){console.log("logDown");}
-
-function logDownWBubblingProtection(event){
-    console.log (logDown);
-    event.stopPropagation()
-}

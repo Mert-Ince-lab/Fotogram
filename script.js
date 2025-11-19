914290b4e3dec7bf4277294a57191ec3ec7b06ec
Mert-Ince-lab <mertince2322@gmail.com>
2025-11-19 11:46:37 +0100

    tried something with JavaScript.

---
+++ script.js
@@ -0,0 +1,9 @@
+let image = (document.getElementById("my-pics").innerHTML =
+    '<img src="./leaves-4702127_1280.jpg"> <img src="./fallow-deer-9903609_1280.jpg"')
+
+
+function openDialog(dialog) {
+    dialog.showModal();
+}
+
+let myDialog = document.getElementById("myDialog");

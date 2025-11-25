58fc9d7c8e12d2801f8a55d7a3636c5632372e48
Mert-Ince-lab <mertince2322@gmail.com>
2025-11-25 09:35:08 +0100

    Chende js

--- script.js
+++ script.js
@@ -1,9 +1,32 @@
-let image = (document.getElementById("my-pics").innerHTML =
-    '<img src="./leaves-4702127_1280.jpg"> <img src="./fallow-deer-9903609_1280.jpg"')
 
 
-function openDialog(dialog) {
-    dialog.showModal();
+function openDialog(src) {
+    document.getElementById("dialogImage") .src = src;
+    myDialog.showModal()
 }
 
 let myDialog = document.getElementById("myDialog");
+
+let images = [
+    "./leaves-4702127_1280.jpg",
+    "./city-9169729_1280.jpg",
+    "./fallow-deer-9903609_1280.jpg",
+    "./river-6175173_1280.jpg",
+    "./sunset-7133867_1280.jpg",
+    "./nature-6766923_1280.jpg",
+    "./NYC-3875817_1280.jpg",
+    "./forest-2942477_1280.jpg",
+    "./desk-3139127_1280.jpg",
+];
+
+function renderImages() {
+    let imageContainer = document.getElementById('image-container');
+
+    for (let index = 0; index < images.length; index++) {
+        imageContainer.innerHTML += `<img src="${images[index]}" onclick="openDialog(myDialog)">`;
+    }
+}
+
+function init() {
+    renderImages();
+}
